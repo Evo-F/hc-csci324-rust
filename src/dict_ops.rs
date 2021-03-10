027ecb296452d0b170c97b7293557a21c0fddf97
evo <thetrueevo@gmail.com>
2021-03-09 19:20:12 -0500

    Add dict_ops.rs - currently contains only test code; will be used to operate on the dictionary for selecting words and adding new ones

---
+++ src/dict_ops.rs
@@ -0,0 +1,22 @@
+//TODO: This file contains code that was borrowed from this link:
+//               https://stackoverflow.com/questions/50788009/how-do-i-get-a-random-line-from-a-file
+// This code is present solely for testing. It needs to be modified and properly cited.
+
+use rand::seq::IteratorRandom;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+// Randomly selects a word from the filepath and returns it as a String.
+pub fn choose_word() -> String {
+    let filename = "src/testdict.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let lines = reader.lines().map(|l| l.expect("Couldn't read lines!"));
+    lines
+        .choose(&mut rand::thread_rng())
+        .expect("Issue with lines!")
+
+}
