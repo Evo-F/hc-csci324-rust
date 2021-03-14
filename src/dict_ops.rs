af5621d09990a1fa66f61ce1dcce4b3040daa8e7
evo <thetrueevo@gmail.com>
2021-03-14 17:14:21 -0400

    Updates dict_ops.rs to support adding new words to dictionary, as well as optimizing file I/O

--- src/dict_ops.rs
+++ src/dict_ops.rs
@@ -4,8 +4,8 @@
 
 use rand::seq::IteratorRandom;
 use std::{
-    fs::File,
-    io::{BufRead, BufReader},
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, BufWriter, Write},
 };
 
 // Randomly selects a word from the filepath and returns it as a String.
@@ -15,11 +15,36 @@ pub fn choose_word() -> String {
     if filename.contains("testdict") {
         println!("DEV NOTICE: You are using the test dictionary!")
     }
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
+
+    //We love OpenOptions!
+    //Attempts to open the specified FilePath in read-only mode.
+    //The .expect() is for error handling.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .open(filename)
+        .expect("ERROR: File not found!");
+
+    let reader = BufReader::new(file); //Standard buffered reader
 
     let lines = reader.lines().map(|l| l.expect("Couldn't read lines!"));
-    lines
+    //Okay, SO.
+    //reader.lines() returns an iterator over the lines in the file - this iterator is a Lines object
+    //This Lines iterator is....kinda useless. So, we use .map() to convert it into an iterator of Strings.
+    //This String iterator (called 'lines') can then be used with .choose() and .expect() below.
+     lines
         .choose(&mut rand::thread_rng())
         .expect("Issue with lines!")
+}
+
+// Appends the word provided as a parameter to the dictionary file
+pub fn add_word(word: &str) {
+
+
+    let filename = "src/testdict.txt";
+
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open(filename)
+        .expect("ERROR: File not found!");
+    writeln!(file, word).expect("ERROR: Could not write to file!");
 }
