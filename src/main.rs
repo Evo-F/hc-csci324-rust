8d535a6435b08da8549e7a91229657e5d59c6ef3
Justin <jebell21@g.holycross.edu>
2021-03-15 00:28:26 -0400

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod dict_ops;
 fn update_game(word:&str, guess_list:&str, strikes:i8)->bool {
 
     let mut solved = true; // initialize the win condition
-    let ascii_array:[&str; 9] = ["_______\n|\n|\n|\n|\n|", // build the gallows
+    let ascii_array:[&str; 9] = [ // store the ASCII art for the game stages
+        "_______\n|\n|\n|\n|\n|", // build the gallows
         "_______\n|    |\n|\n|\n|\n|", // hang the rope
         "_______\n|    |\n|   ( )\n|\n|\n|", // draw the head
         "_______\n|    |\n|   ( )\n|    |\n|\n|", // draw the torso
@@ -14,8 +15,10 @@ fn update_game(word:&str, guess_list:&str, strikes:i8)->bool {
         "_______\n|    |\n|   ( )\n|   /|\\\n|\n|", // draw the right arm
         "_______\n|    |\n|   ( )\n|   /|\\\n|   /\n|", // draw the left leg
         "_______\n|    |\n|   ( )\n|   /|\\\n|   / \\\n|", // draw the right leg
-        "_______\n|    |\n|   (_)\n|   /|\\\n|   / \\\n|"]; // hang the hangman
+        "_______\n|    |\n|   (_)\n|   /|\\\n|   / \\\n|" // hang the hangman
+    ];
     println!("{}", ascii_array[strikes as usize]); // print out the appropriate hangman stage
+
     print!("> ");
     for char in word.chars() { // print out the word so far
         if guess_list.contains(char) { // for each character that has been previously guessed
@@ -26,93 +29,154 @@ fn update_game(word:&str, guess_list:&str, strikes:i8)->bool {
             solved = false; // if there are any blank spaces, the game has not been won yet
         }
     }
-    println!(); // if no spaces are blank, the word is solved - return true
+    println!();
+
+    if solved { // if no spaces are blank, the word is solved - return true
+        return solved;
+    }
+    let mut guess_list: Vec<char> = guess_list.chars().collect();
+    guess_list.sort();
+    guess_list.dedup();
+    print!("Used Letters: ");
+    for letter in guess_list {
+        print!("{} ", letter);
+    }
+    println!("\nLives Remaining: {}", 8 - strikes);
     return solved; // else return false
 }
 
+fn menu_select() -> u32 {
+    println!();
+    println!("_____________");
+    println!("|          |");
+    println!("|  ( H A N G M A N )");
+    println!("|");
+    println!("| (1) Play Game");
+    println!("| (2) Set Difficulty");
+    println!("| (3) Add Custom Words");
+    println!("| (4) Quit");
+    println!("|");
+    print!("Select an Option (1-4): ");
+    return get_menu_input(4, "Select a Valid Option (1-4): ");
+}
 
-fn main() {
+fn diff_select() -> u32 {
+    println!();
+    println!("D I F F I C U L T Y");
+    println!("(1) Easy");
+    println!("(2) Medium");
+    println!("(3) Hard");
+    println!("(4) Random");
+    print!("Select a Difficulty (1-4): ");
+    io::stdout().flush().unwrap();
+    return get_menu_input(4, "Select a Valid Difficulty (1-4): ");
+}
 
-    // GAME LOOP - Continuously reset the game until the user wishes to stop.
+fn get_menu_input(num_options:u32, err_message:&str) -> u32{
     loop {
-
-        /*print!("1 - Easy\n2 - Medium\n3 - Hard\nSelect a difficulty (1-3): ");
         io::stdout().flush().unwrap();
-        let mut diff = String::new();
-        std::io::stdin().read_line(&mut diff).unwrap();
-        let diff = diff.trim().parse::<u32>().unwrap();*/
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        if input.len() > 2 {
+            print!("{}", err_message);
+            continue;
+        }
+        let input = input.chars().next().unwrap();
+        if !input.is_numeric() {
+            print!("{}", err_message);
+            continue;
+        }
+        let input = input.to_digit(10).unwrap();
+        if input > num_options || input < 1 {
+            print!("{}", err_message);
+            continue;
+        }
+        return input;
+    }
+}
 
-        // INITIALIZE ALL VARIABLES
-        let mut strikes:i8 = 0; // number of strikes, 7 strikes triggers loss condition
+fn play_game(difficulty:u32) {
+
+    loop { // GAME LOOP - Loop until player chooses to quit to main menu.
+
+        let mut strikes:i8 = 0; // number of strikes, 8 strikes triggers loss condition
         let mut guess_list:String = "".to_owned(); // string of past player guesses
-        let word:&str = &dict_ops::choose_word(1).to_lowercase(); //choose_word() returns String, needs to be borrowed
-        //let dev_mode = true;
-        //if dev_mode {
-        //    println!("DEV MODE IS ENABLED. DEV MESSAGES WILL BE PRINTED.");
-        //    println!("THE WORD IS: {}", word);
-        //}
+        let word:&str = &dict_ops::choose_word(difficulty).to_lowercase(); //choose_word() returns String, needs to be borrowed
+
+        println!("THE WORD IS: {}", word);
+
         update_game(word, &guess_list, strikes); // call function to print initial board
 
-        // INPUT LOOP - Continuously prompt user for input until the win/loss condition is met.
-        loop {
+        loop { // INPUT LOOP - Continuously prompt user for input until the win/loss condition is met.
 
             // PROMPT USER INPUT
-            print!("Enter your guess: ");
+            print!("Your Guess: ");
             io::stdout().flush().unwrap();
-            let mut guess = String::new();
-            std::io::stdin().read_line(&mut guess).unwrap();
-            let guess = guess.trim();
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let input = input.trim();
 
-            if guess.len() > 1 { // If the user input a string, ignore all but the first character.
-                if word == guess {
-                    guess_list.push_str(&guess);
-                    println!("'{}' is the correct word!", guess);
+            if input.len() > 1 { // If the user input a string, ignore all but the first character.
+                //print!("You guessed the word '{}'. ", input);
+                if word == input {
+                    guess_list.push_str(&input);
+                    println!("This is the correct word!");
                 } else {
-                    println!("'{}' is not the correct word.", guess);
+                    println!("This is not the correct word.");
                     strikes += 1;
                 }
-            } // end of if guess.len() > 1
+            } // end of if input.len() > 1
             else {
-                let guess = guess.to_ascii_lowercase().chars().next().unwrap(); // only use the first character
-
+                let input = input.to_ascii_lowercase().chars().next().unwrap(); // only use the first character
+                //print!("You guessed '{}'. ", input);
                 // INPUT RESULTS - Update guess_list with new input and inform player of input correctness.
-                if !guess.is_alphabetic() { // if the character is not a letter, ignore it
-                    println!("'{}' is not a valid letter.", guess);
-                } else if guess_list.contains(guess) { // if the character has already been guessed, ignore it
-                    println!("'{}' has already been guessed.", guess);
-                } else if word.contains(guess) { // if the character is contained in the word
-                    guess_list.push(guess); // push it to the guess_list
-                    println!("'{}' was a correct guess.", guess);
+                if !input.is_alphabetic() { // if the character is not a letter, ignore it
+                    println!("This is not a valid letter.");
+                } else if guess_list.contains(input) { // if the character has already been guessed, ignore it
+                    println!("You have already guessed this letter.");
+                } else if word.contains(input) { // if the character is contained in the word
+                    guess_list.push(input); // push it to the guess_list
+                    println!("This letter is part of the word.");
                 } else { // if the character was an incorrect guess
-                    guess_list.push(guess); // push it to the guess list
+                    guess_list.push(input); // push it to the guess list
                     strikes += 1; // and give player a strike for their mistake
-                    println!("'{}' was not a correct guess.", guess);
+                    println!("This letter is not part of the word.");
                 }
             } // end of else
 
             // GAME RESULTS - End game if player has max number of strikes, or if the word has been solved.
             if update_game(word, &guess_list, strikes) {
-                println!("THE MAN IS SAVED");
+                println!("The man is saved! You guessed the word.");
                 break;
             } else if strikes >= 8 {
-                println!("THE MAN IS HANGED\nThe correct word was '{}'", word);
+                println!("The man is hanged! The correct word was '{}'.", word);
                 break;
             }
         } // end of input loop
 
-        print!("Play Again? (y/n) ");
-        io::stdout().flush().unwrap();
-        let mut play_again = String::new();
-        std::io::stdin().read_line(&mut play_again).unwrap();
-        let play_again = play_again.trim();
-        if play_again == "y" {
-            println!("Here we go again!");
-            continue;
+        // PLAY AGAIN?
+        println!("(1) Play Again");
+        println!("(2) Return to Menu");
+        print!("Select an Option (1-2): ");
+        match get_menu_input(2, "Select a Valid Option (1-2): ") {
+            1 => continue, // restarts game with current settings
+            _ => break // leaves play_game() and returns to menu
         }
-        else {
-            println!("Thanks for playing!");
-            break;
-        }
-
     } // end of game loop
+}
+
+fn main() {
+
+    let mut difficulty:u32 = 4; // difficulty set to "Random" by default
+        loop { // MENU LOOP - Player stays here until they select (4) Quit
+            println!("[Dev] Difficulty: {}", difficulty);
+
+            match menu_select() {
+                1 => play_game(difficulty), // start game with current settings, returns when game ends
+                2 => difficulty = diff_select(), // select difficulty, returns when diff selected
+              //3 => // adds custom word to dictionary, returns when user backs out
+                _ => break // quits game, ends main function
+            }
+        }
+    println!("Thanks for playing!");
 }
