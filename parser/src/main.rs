78eb37e586f5fadb737e87a158f479cf6f3c3adf
evo <thetrueevo@gmail.com>
2021-03-16 23:33:06 -0400

    Adds code for unit testing

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,3 +1,28 @@
+mod logic;
+
 fn main() {
-    println!("Hello world!");
+    //We are going to test the parsing of "a AND b."
+    let a = false;
+    let b = false;
+    let a_log = logic::LogicObject {
+        mode: 0, //TERMINAL
+        subsidiaries: Vec::new(),
+        root: &a,
+    };
+
+    let b_log = logic::LogicObject {
+        mode: 0, //TERMINAL
+        subsidiaries: Vec::new(),
+        root: &b,
+    };
+
+    let root_log = logic::LogicObject {
+        mode: 1, //AND
+        subsidiaries: vec![a_log, b_log],
+        root: &false,
+    };
+
+    let result = logic::evaluate(&root_log);
+    println!("{}", result); //should evaluate to false
+
 }
