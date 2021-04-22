8042c6204d03098d0a4831b2815e2e7e97f86ae7
evo <thetrueevo@gmail.com>
2021-04-22 14:49:24 -0400

    Completely revamps how we handle the logical structure of the user input using tuples and HashMaps

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,33 +1,92 @@
-mod logic;
+use std::collections::HashMap;
 
 //test comment
 
 fn main() {
     //We are going to test the parsing of "a AND b."
     //STILL WORKING ON THIS.
-    unsafe {
-        let mut a_log = logic::LogicObject {
-            mode: 0,
-            subsidiaries: Vec::new(),
-            root: false
-        };
 
-        let mut b_log = logic::LogicObject {
-            mode: 0,
-            subsidiaries: Vec::new(),
-            root: false
-        };
+    let mut items: HashMap<(String, i32), (i32, (String, i32), (String, i32))> = HashMap::new();
+    let mut terms: HashMap<(String, i32), bool> = HashMap::new();
 
-        let mut and_log = logic::LogicObject {
-            mode: 0,
-            subsidiaries: Vec::new(),
-            root: false
-        };
+    let a_key: (String, i32) = ("a".parse().unwrap(), 0);
+    let b_key: (String, i32) = ("b".parse().unwrap(), 0);
+    let and_key: (String, i32) = ("&".parse().unwrap(), 0);
 
-        print!("{}", logic::evaluate(&and_log));
+    items.insert(a_key.clone(), (0, a_key.clone(), a_key.clone()));
+    items.insert(b_key.clone(), (0, b_key.clone(), b_key.clone()));
+    items.insert(and_key.clone(), (1, a_key.clone(), b_key.clone()));
+
+    terms.insert(a_key.clone(), false);
+    terms.insert(b_key.clone(), false);
+
+    let eval_tuple = items.get(&and_key);
+    if eval_tuple.is_none() {
+        println!("Oops! Something went wrong!")
+    } else {
+        // EVAL 1 - Both false.
+        let eval_tuple_real = eval_tuple.unwrap();
+
+        println!("{}", evaluate(eval_tuple_real.clone(), &items, &terms));
+
+        // EVAL 2 - Both true.
+        terms.insert(a_key.clone(), true);
+        terms.insert(b_key.clone(), true);
+        println!("{}", evaluate(eval_tuple_real.clone(), &items, &terms));
+
+        // EVAL 3 - A false, B true.
+        terms.insert(a_key.clone(), false);
+        println!("{}", evaluate(eval_tuple_real.clone(), &items, &terms));
+
+        // EVAL 4 - A true, B false.
+        terms.insert(a_key.clone(), true);
+        terms.insert(b_key.clone(), false);
+        println!("{}", evaluate(eval_tuple_real.clone(), &items, &terms));
+
 
     }
 
-
-
 }
+
+fn evaluate(tuple: (i32, (String, i32), (String, i32)),
+            items: &HashMap<(String, i32), (i32, (String, i32), (String, i32))>,
+            terms: &HashMap<(String, i32), bool>) -> bool {
+    let (mode, a, b) = tuple;
+
+    let mut a_temp;
+    let mut b_temp;
+
+    return if mode == 0 {
+        // This means we've reached a terminal and just need to get its value.
+        // The b element is not needed, as terminals point to themselves in closed loops.
+        let a_temp = terms.get(&a);
+
+        if a_temp.is_none() {
+            return false;
+        }
+
+        *a_temp.unwrap()
+    } else {
+        let mut a_bool = false;
+        let mut b_bool = false;
+
+        a_temp = items.get(&a);
+        b_temp = items.get(&b);
+
+        if a_temp.is_none() || b_temp.is_none() {
+            return false;
+        }
+
+        a_bool = evaluate(a_temp.unwrap().clone(), &items, &terms);
+        b_bool = evaluate(b_temp.unwrap().clone(), &items, &terms);
+
+        match mode {
+            1 => a_bool && b_bool, // AND
+            2 => a_bool || b_bool, // OR
+            3 => !a_bool,          // NOT
+            4 => a_bool == b_bool, // EQUIVALENCE
+            _ => a_bool
+        }
+    }
+}
+
